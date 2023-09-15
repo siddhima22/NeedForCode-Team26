c0110b0ee34e9813f8873ab75ebd9a38c28fd237
mahimavarma <mahimavarma1609@gmail.com>
2023-09-15 13:15:42 +0530

    commit message

---
+++ app.js
@@ -0,0 +1,25 @@
+function calculatorIncomeTax(income){
+    let tax= 0;
+    if(income <= 300000){
+        tax = 0;
+    }else if(income > 300000 && income <= 600000){
+        tax = (income - 300000) *0.05;
+    }else if(income > 600000 && income <= 900000){
+        tax = (300000) * 0.05 + (income - 600000) * 0.10
+    }else if(income > 900000 && income <= 1200000){
+        tax = (300000) * 0.05 + (300000) * 0.10 + (income - 900000) * 0.15
+    }else if(income > 1200000 && income <= 1500000){
+        tax = (300000) * 0.05 + (300000) * 0.10 + ( 300000) * 0.15 + (income - 1200000) * 0.20
+    }else{
+        tax = (300000 * 0.05) + (300000 * 0.10) +  (300000 * 0.15) + (300000 * 0.20) + (income - 1500000) * 0.30
+    }
+    return tax;
+}
+const Form = document.getElementById('tax-form')
+Form.addEventListener('submit', (event)=>{
+    event.preventDefault();
+
+    const income = Number(Form.income.value);
+    const tax = calculatorIncomeTax(income);
+    document.getElementById('infoTextInput').value = tax;
+})
